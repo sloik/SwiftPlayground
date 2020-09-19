7bd047e75180305bf48e2432d9dbfa03c0a0ddb5
Ed Maliniak <>
2020-09-19 21:44:56 +0200

    Minor formatting and notes.

--- contents/swift_from_zero_to_hero.playground/Pages/07_03_currying.xcplaygroundpage/Contents.swift
+++ contents/swift_from_zero_to_hero.playground/Pages/07_03_currying.xcplaygroundpage/Contents.swift
@@ -3,10 +3,10 @@
 
 import Foundation
 
-//: Rozwijanie Funkcji to technika przy użyciu której jedna metoda zwraca inną ale z mniejszą ilością parametrów. Z czego ta "wewnętrzna metoda" ma dostęp do wszystkich wcześniej podanych.
+//: Rozwijanie Funkcji to technika przy użyciu której jedna metoda zwraca inną ale z mniejszą ilością parametrów. Z czego ta "wewnętrzna metoda" ma dostęp do wszystkich wcześniej podanych parametrów.
 
 
-func addToCartNormal(_ product: String, price: Double) -> ((Int) -> String) {
+func addToCartNormal(_ product: String, price: Double) -> (Int) -> String {
 
     func discountFunction(_ count: Int) -> String {
         let discountAmount: Double
@@ -26,8 +26,10 @@ func addToCartNormal(_ product: String, price: Double) -> ((Int) -> String) {
     return discountFunction
 }
 
-let itemToBuy = addToCartNormal("Złote Galoty", price: 100)
-print(itemToBuy(5))
+run ("📀 Golden Pants"){
+    let itemToBuy = addToCartNormal("Złote Galoty", price: 100)
+    print(itemToBuy(5))
+}
 
 
 /*:
@@ -61,16 +63,24 @@ let brownPantiesWaitingForPriceInformationFunction = addCurriedVersion("Brązowe
 `brownPanties...` ma w sobie zapieczoną informację o produkcie. Jest to bezpiecznie zamknięte wewnątrz bez możliwości podmiany tego produktu. W następnym kroku powiemy tej funkcji jaka jest cena za jedną sztukę produktu i w zamian otrzymamy kolejną funkcje!
  */
 
-let calculatePriceWithDiscountWaitingForItemsCount = brownPantiesWaitingForPriceInformationFunction(100)
+run ("💰 curry version") {
+    let calculatePriceWithDiscountWaitingForItemsCount = brownPantiesWaitingForPriceInformationFunction(100)
+    
+    print(
+        calculatePriceWithDiscountWaitingForItemsCount(1),
+        calculatePriceWithDiscountWaitingForItemsCount(5),
+        calculatePriceWithDiscountWaitingForItemsCount(15),
+        calculatePriceWithDiscountWaitingForItemsCount(55),
+        separator: "\n"
+    )
+}
 
-print(
-    "-",
-    calculatePriceWithDiscountWaitingForItemsCount(1),
-    calculatePriceWithDiscountWaitingForItemsCount(5),
-    calculatePriceWithDiscountWaitingForItemsCount(15),
-    calculatePriceWithDiscountWaitingForItemsCount(55),
-    separator: "\n"
-)
-
+/*:
+ Dostajemy kolejny sposób na _enkapsulacje_, ukrywanie detali implementacyjnych.
+ 
+ Curring jest obecny w wielu językach, szczególnie tych nastawionych na programowanie funkcyjne. Dzięki takiemu podejściu dużo łatwiej komponuje się ze sobą funkcje tworząc z mniejszych większe.
+ 
+ Dogłębnie wchodzimy w ten temat na naszym YouTube-owym kanale w play-liście [Lekko Technologiczny - Funkcyjny Swift](https://www.youtube.com/playlist?list=PLk_5PV9LrXp-R6TM86MxqlihQSu_ZIhUk).
+ */
 
 //:[ToC](00-00_toc) | [Tips and Tricks](900-00-tips_and_tricks) | [Previous](@previous) | [Next](@next)
