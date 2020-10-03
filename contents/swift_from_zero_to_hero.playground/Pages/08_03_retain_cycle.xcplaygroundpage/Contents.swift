1fabc5bd07e2c906eeb2847a67f846ce0240380f
dominika-kokowicz <50965392+dominika-kokowicz@users.noreply.github.com>
2020-10-03 11:13:18 +0200

    Fixing typos.
    Fixing typos.

--- contents/swift_from_zero_to_hero.playground/Pages/08_03_retain_cycle.xcplaygroundpage/Contents.swift
+++ contents/swift_from_zero_to_hero.playground/Pages/08_03_retain_cycle.xcplaygroundpage/Contents.swift
@@ -95,7 +95,7 @@ run("👗 No leaking memory") {
 /*:
  ## Bloki
  
- Ponieważ "łapią" obiekty w dostępnym zakresie (scope), **również mogą spowodować retain cycle**. W miejscu gdzie w bloku używamy jakiejś zmiennej z poza bloku kompilator tworzy i "dowiązuje" specjalny obiekt, który jest używany do "złapania" referencji lub użytych wartości.
+ Ponieważ "łapią" obiekty w dostępnym zakresie (scope), **również mogą spowodować retain cycle**. W miejscu gdzie w bloku używamy jakiejś zmiennej spoza bloku kompilator tworzy i "dowiązuje" specjalny obiekt, który jest używany do "złapania" referencji lub użytych wartości.
  
  > Działa to tak, że kompilator w miejscu użycia bloku generuje _ukrytą_ klasę i tworzy jej instancje. Wszystkie obiekty, jakie są użyte wewnątrz stają się _property_ tej klasy. To też mam nadzieje _wyjaśnia_ dlaczego można się spotkać ze stwierdzeniem, że _bloki to obiekty_. [Stack: how are nsblocks objects created](https://stackoverflow.com/questions/20134616/how-are-nsblock-objects-created) i [Implementacja NSBlock w ObjC](https://github.com/nst/iOS-Runtime-Headers/blob/master/Frameworks/CoreFoundation.framework/NSBlock.h)
  
