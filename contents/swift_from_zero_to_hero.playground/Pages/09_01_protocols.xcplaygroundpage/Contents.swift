2ea57db032eeda32e164dd3b7f8f587902562750
Ed Maliniak <>
2021-02-20 16:09:12 +0100

    Adds comment.

--- contents/swift_from_zero_to_hero.playground/Pages/09_01_protocols.xcplaygroundpage/Contents.swift
+++ contents/swift_from_zero_to_hero.playground/Pages/09_01_protocols.xcplaygroundpage/Contents.swift
@@ -1,7 +1,7 @@
 //:[ToC](00-00_toc) | [Tips and Tricks](900-00-tips_and_tricks) | [Previous](@previous) | [Next](@next)
 //: ## [Protocols](https://developer.apple.com/library/ios/documentation/Swift/Conceptual/Swift_Programming_Language/Protocols.html)
 /*:
- Najłatwiej myśleć o protokołach jak o kontrakcie zawierającym wymagania, które adoptujący typ (struktura/klasa/typ prosty) "obiecuje" spełnić. Jeżeli taka sytuacja nastąpi to mówimy, że dany typ implementuje dany protokół.
+ Najłatwiej myśleć o protokołach jak o kontrakcie zawierającym wymagania, które adoptujący typ (struktura/klasa/typ prosty) "obiecuje" spełnić. Jeżeli taka sytuacja nastąpi to mówimy, że dany typ implementuje (ang. conforms to) dany protokół.
  
  W definicji protokołu mogą się znajdować metody jak rownież i właściwości oraz domyślne implementacje tych metod. Dodatkowo protokoły mogą dziedziczyć po sobie. Co pozwala na komponowanie protokołów (łączenie kliku w jeden).
  */
