382fd7ec9da72faf58eafa133fe6d716cf9b030b
Dominika Kokowicz <50965392+dominika-kokowicz@users.noreply.github.com>
2021-08-27 17:55:14 +0200

    Update Contents.swift
    Fixed typos

--- contents/swift_from_zero_to_hero.playground/Pages/12_00_Access_Control.xcplaygroundpage/Contents.swift
+++ contents/swift_from_zero_to_hero.playground/Pages/12_00_Access_Control.xcplaygroundpage/Contents.swift
@@ -6,8 +6,8 @@ W Swift modyfikatory dostępu występują na dwóch poziomach: pliku oraz moduł
 
 Wystepują trzy modyfikatory:
 * **public** - Najszerszy ze wszystkich świetnie się nadaje do definiowania API framework-ów. Wszystkie symbole w ten sposób oznaczone są dostępne w całym module jak również można je importować w innych modułach.
-* **internal** - **Domyślny dla Projektów Xcode**. Symbole są dostępne w całym projekcie natomiast nie mogą być zaimporotwane w innym pliku.
-* **private** -**Domyślny dla Placu Zabaw**. Symbole są dostępne tylko **w pliku** w którym są zdefiniowane.
+* **internal** - **Domyślny dla Projektów Xcode**. Symbole są dostępne w całym projekcie natomiast nie mogą być zaimportowane w innym pliku.
+* **private** - **Domyślny dla Placu Zabaw**. Symbole są dostępne tylko **w pliku** w którym są zdefiniowane.
 */
 
 private class PrivateClass {
@@ -22,7 +22,7 @@ class JustSomeClass {
 }
 
 /*:
-Ponieważ większość kodu jaki jest pisany ląduje zdecydownaie w projekcie Xcode to zapraszam do otworzenia **AccesControl** który się znajduje obok tego placu zawbaw.
+Ponieważ większość kodu jaki jest pisany ląduje zdecydowanie w projekcie Xcode to zapraszam do otworzenia **AccessControl** który się znajduje obok tego placu zabaw.
 */
 
 //:[ToC](00-00_toc) | [Tips and Tricks](900-00-tips_and_tricks) | [Previous](@previous) | [Next](@next)
